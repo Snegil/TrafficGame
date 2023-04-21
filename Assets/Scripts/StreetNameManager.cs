80fb355d333a0755578058a4fb7130ca5cb0d117
Alfons <144wallfin@gmail.com>
2023-04-21 12:15:50 +0200

    Street names added to colliders and displays it
    Red buildings added aswell and small changes to blue and green

---
+++ Assets/Scripts/StreetNameManager.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class StreetNameManager : MonoBehaviour
+{
+    [SerializeField]
+    LayerMask layerMask;
+
+    Camera cam;
+
+    [SerializeField]
+    TextMeshProUGUI text;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 mousePosition = Mouse.current.position.value;
+
+        Vector2 mouseWorldPos = cam.ScreenToWorldPoint(mousePosition);
+
+        Collider2D[] roads = Physics2D.OverlapPointAll(mouseWorldPos, layerMask);
+
+        text.text = " ";
+        if (roads != null)
+        {
+            if (roads.Length == 1)
+            {
+                text.text = roads[0].name;
+            }
+            if (roads.Length == 2)
+            {
+                text.text = "Junction of " + roads[0].name + " and " + roads[1].name;
+            }
+        }
+    }
+}
